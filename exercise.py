506445802c9ae3e6183cb8de503f9040412a6f38
angelless00 <angelless00@naver.com>
2024-07-02 16:40:01 +0900

    05

--- exercise.py
+++ exercise.py
@@ -1,8 +1,2 @@
-age=int(input())
-balance=9000
-if age>=7 and age<=12:
-    print(balance-650)
-elif age>=13 and age<=18:
-    print(balance-1050)
-else:
-    print(balance-1250)
+msg=input('문자열을 입력하시오 : ')
+print(len(msg))d
