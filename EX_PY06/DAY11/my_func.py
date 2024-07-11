352b09c3065a7fb378002a464db4f48fdc2e97ee
angelless00 <angelless00@naver.com>
2024-07-11 18:20:34 +0900

    11

--- EX_PY06/DAY11/my_func.py
+++ EX_PY06/DAY11/my_func.py
@@ -1,4 +1,13 @@
 def add(n1,n2): return n1+n2
 
-print("TEST")
-print(f'add=>{add(10,3)}')
+
+if __name__=='__main__':
+    print(__name__)
+    print("TEST")
+    print(f'add=>{add(10,3)}')
+
+
+
+
+
+
