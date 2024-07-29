842ccd518397d96227a76e0316b23b2e328dcec1
angelless00 <angelless00@naver.com>
2024-07-29 22:53:35 +0900

    22

---
+++ excercise.py
@@ -0,0 +1,7 @@
+students=[('Alice',3.9,20160303),
+          ('Bob',3.0,20160302),
+          ('Charlie',4.3,20160301)]
+
+# 학번(students[2])을 기준으로 오름차순 정렬
+sorted_studen1=students[2]
+print(sorted_studen1)
