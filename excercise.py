d4fb81cdaeb99a16cf496b6dc3f8c90e663f6705
angelless00 <angelless00@naver.com>
2024-08-05 08:55:40 +0900

    27

--- excercise.py
+++ excercise.py
@@ -1,18 +1,6 @@
-Table={'Seoul':['South Korea','Asia',9655000],
-       'Tokyo':['Japan','Asia',14110000],
-       'Beijing':['China','Asia',21540000],
-       'London':['United Kingdom','Europe',14800000],
-       'Berlin':['Germany','Europe',3426000],
-       'Mexico City':['Mexico','America',21200000]}
+def game():
+    row=1
+    colmn=2
+    return row,colmn
 
-Table=sorted(Table,key=lambda x: x[i][2] for i in Table)
-
-print(Table)
-
-
-
-
-
-
-
-
+print(game())
